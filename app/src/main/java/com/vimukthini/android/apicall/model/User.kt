ad0543e731cca973a787223c2e9d0048ab49d14e
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2022-03-21 09:13:13 +0530

    first kotlin project

---
+++ app/src/main/java/com/vimukthini/android/apicall/model/User.kt
@@ -0,0 +1,9 @@
+package com.vimukthini.android.apicall.model
+
+data class User(
+    val id : Number,
+    val name: String,
+    val username:String,
+    val email:String,
+    val website:String
+    )
